b3595faf026deb0d44d7c6d36411978ff32d9e53
s.fedorov <s.fedorov@propellerads.net>
2021-01-24 22:53:39 +0300

    create game on Unity

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+   public float speed = 5f;
+   private Transform _rorator;
+
+    private void Start()
+    {
+        _rorator = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        _rorator.Rotate(0, speed * Time.deltaTime, 0);
+    }
+
+}
